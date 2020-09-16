d799e0705be37eed1093ac2b3263bc55709ba4fb
Guido <guido.amodio@gmail.com>
2020-09-16 15:04:48 +0200

    snack2

---
+++ Snack2/mail.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+
+if(strlen($name) < 3 || empty($mail) ||  is_numeric($age) == false){
+    $text = 'accesso negato';
+} else {
+    // var_dump(strpos($mail,'@'));
+    //
+    if((strpos($mail,'@') !== false) && (strpos($mail,'.') !== false)){
+    $text = $name . $mail . $age;
+    } else {
+    $text = ' controlla la mail';
+    }
+echo $text;
+}
+
+
+?>
