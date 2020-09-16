5ccefda6a0052042f8cad30e06407d9105b384c1
Guido <guido.amodio@gmail.com>
2020-09-16 16:13:18 +0200

    last

--- Snack2/mail.php
+++ Snack2/mail.php
@@ -3,12 +3,16 @@ $name = $_GET['name'];
 $mail = $_GET['mail'];
 $age = $_GET['age'];
 
+if(strlen($name) <3){
+    echo 'il nome è corto';
+}
+
 
 if(strlen($name) < 3 || empty($mail) ||  is_numeric($age) == false){
     $text = 'accesso negato';
 } else {
     if((strpos($mail,'@') !== false) && (strpos($mail,'.') !== false)){
-    $text = $name . $mail . $age;
+    $text = 'accesso riuscito';
     } else {
     $text = ' controlla la mail';
     }
